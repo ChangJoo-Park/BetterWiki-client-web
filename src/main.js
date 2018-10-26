e958f8bcb8b8fc2a3e4e60933b3e5fe02c5a486d
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-26 23:21:54 +0900

    Add vue dependencies in main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import BaseModel from '@/models/Base'
 Vue.use(BootstrapVue)
-
 Vue.config.productionTip = true
+
+BaseModel.httpClient = axios
+BaseModel.baseURL = '/api'
 
 new Vue({
   router,
