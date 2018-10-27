f9cddf12a971f8fef72272d940b880805d8ff9fb
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-27 13:49:58 +0900

    Add setup view component

---
+++ src/views/Setup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="checkSetup">
+    <div v-if="needSetup">
+      <h1>Hello World</h1>
+    </div>
+    <div v-else>
+      <h1>You need not any setup</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Service from '@/models/Service'
+
+export default {
+  data () {
+    return {
+      checkSetup: false,
+      needSetup: false
+    }
+  },
+
+  created () {
+    Service.isSetup()
+      .then(({ needSetup }) => {
+        this.checkSetup = true
+        this.needSetup = needSetup
+      })
+  }
+}
+</script>
+
+<style>
+</style>
