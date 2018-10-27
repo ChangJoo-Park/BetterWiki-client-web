0d15bd3d0e0b8fe47c66b8e6abf2e1de0e1ea652
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-10-27 15:16:47 +0900

    Add user and service store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: null,
+    service: null
   },
   mutations: {
-
+    SET_USER (state, user) {
+      state.user = user
+    },
+    SET_SERVICE (state, service) {
+      state.service = service
+    }
   },
   actions: {
-
+    setUser ({ commit }, user) {
+      commit('SET_USER', user)
+    },
+    setService ({ commit }, service) {
+      commit('SET_SERVICE', service)
+    }
+  },
+  getters: {
+    user: (state) => state.user,
+    service: (state) => state.service
   }
 })
