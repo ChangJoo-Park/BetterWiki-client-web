711cd2f80e485c52d06d8ee12039a859a35d51af
ChangJoo Park <pcjpcj2@gmail.com>
2018-11-01 23:12:28 +0900

    Remove viewer

--- src/views/ArticleNew.vue
+++ src/views/ArticleNew.vue
@@ -32,12 +32,11 @@
 import 'tui-editor/dist/tui-editor.css'
 import 'tui-editor/dist/tui-editor-contents.css'
 import 'codemirror/lib/codemirror.css'
-import { Editor, Viewer } from '@toast-ui/vue-editor'
+import { Editor } from '@toast-ui/vue-editor'
 
 export default {
   components: {
-    Editor,
-    Viewer
+    Editor
   },
   data () {
     return {
