36fc8885ecc215ac657cb7f12541831fc3b16779
ChangJoo Park <pcjpcj2@gmail.com>
2018-11-03 11:50:14 +0900

    Add article save page

--- src/views/ArticleNew.vue
+++ src/views/ArticleNew.vue
@@ -3,16 +3,26 @@
     <h1>New Article</h1>
     <div class="form-group">
       <input
-        v-model="title"
+        v-model.trim="newArticle.title"
         type="text"
         class="form-control"
         placeholder="Awesome infomation"
+        @keydown.tab.prevent.stop="focusEditor"
       >
     </div>
     <editor
-      v-model="body"
+      ref="editor"
+      v-model="newArticle.body"
       height="500px"/>
     <br>
+    <div class="form-group">
+      <input
+        v-model.trim="newArticle.topic"
+        type="text"
+        class="form-control"
+        placeholder="Topic"
+      >
+    </div>
     <input
       type="button"
       class="btn btn-primary"
@@ -26,21 +36,33 @@ import 'tui-editor/dist/tui-editor.css'
 import 'tui-editor/dist/tui-editor-contents.css'
 import 'codemirror/lib/codemirror.css'
 import { Editor } from '@toast-ui/vue-editor'
+import Article from '@/models/Article'
 
 export default {
   components: {
     Editor
   },
-  data () {
+  data() {
     return {
-      whatChanged: '',
-      title: '',
-      body: ''
+      newArticle: {
+        title: '',
+        body: '',
+        topic: ''
+      }
     }
   },
   methods: {
-    submit () {
-      console.log( 'hello world' )
+    async submit() {
+      const savedArticle = await Article.save(Object.assign({}, this.newArticle))
+      this.$router.push({ name: 'article', params: { articleId: savedArticle.id } })
+    },
+    focusEditor() {
+      try {
+        this.$refs[ 'editor' ].editor.mdEditor.cm.focus()
+        this.$refs[ 'editor' ].$el.querySelector('.te-editor-section').focus()
+      } catch (error) {
+        console.error(error)
+      }
     }
   }
 }
